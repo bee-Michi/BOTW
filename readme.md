ba58867db3236376ba286c3ebdc315614f763451
bee-Michi <96332875+bee-Michi@users.noreply.github.com>
2021-12-21 16:12:45 +0100

    v 0.3
    Heyyyy

---
+++ readme.md
@@ -0,0 +1,55 @@
+# Botw!
+
+Welcome to botw or best over time waster.
+
+## Installation
+
+Download the source code then grab the latest release of python:
+
+MacOS: [here](https://www.python.org/downloads/macos/)
+
+Windows: [here](https://www.python.org/downloads/windows/)
+
+Linux: check your [package manger](https://www.geeksforgeeks.org/how-to-install-python-on-linux/)
+
+## Running the softwere
+
+There are two main ways to run the softwere:
+
+### Terminal way:
+
+After installation, go to the directory of the file, and run 
+
+``` python3 botw.py```
+
+### Ide, text editor way:
+
+Open the text editor, and then run the softwere from there
+
+---
+
+## Liscence:
+
+This softere is liscenced under the mit liscence, more information can be found in the liscence file!
+
+## Contribuiting:
+
+You are free to contribute to my terrible code using pull request, thogu if you want to add a feature, open an issue!
+
+## Bug reporting:
+
+Open an issue and describe the problem! Thoug bugs are rear and few, we wuold apprecieate if you reported them!
+
+## Changes:
+
+If you want to learn about the new features of the softwere, go to changes.txt.
+
+## Creator:
+
+This project was created by:
+
+- bee_Michi
+
+contributors can be found in the countribiturs.txt file!
+
+# Enjoy playing!!
---
+++ src/botw.py
@@ -0,0 +1,242 @@
+import time, sys, random, math
+from datetime import datetime
+print("Loadng recorces...")
+time.sleep(1)
+print("Conneccting to servers...")
+time.sleep(1)
+print("Retrivng memory info...")
+time.sleep(2.3)
+__copyrigth__ = "©bee_Michi 2021-2022"
+__author__ = "bee_Michi"
+__version__ = "0.2"
+print("Welcome to BOTW or best over time waster")
+print("Type help to get a list abot all available utilities")
+print("Copyrigth can be acces with the copyrigth command")
+print("And the current version can be accesed with the version command")
+print("Or input exit to exit")
+BOTW_SELECT = input()
+
+countdownS = False
+quiz = False
+calculator = False
+converter = False
+gtn = False
+clock = False
+paswwordG = False
+available = ["countdown", "quiz", "calculator", "converter", "guess the number", "clock", "password generator"]
+while True:
+    if BOTW_SELECT == "copyrigth":
+        print(__copyrigth__)
+        break
+    elif BOTW_SELECT == "version":
+        print(__version__)
+        break
+    elif BOTW_SELECT == "exit":
+        sys.exit()
+    elif BOTW_SELECT == "countdown":
+        countdownS = True
+        break
+    #!Quiz is broken, therphore no acces unless modifictation!
+    elif BOTW_SELECT == "calculator":
+        calculator = True
+        break
+    elif BOTW_SELECT == "converter":
+        converter = True
+        break
+    elif BOTW_SELECT == "help":
+        for i in available:
+            print(i)
+        break
+    elif BOTW_SELECT == "guess the number":
+        gtn = True
+        break
+    elif BOTW_SELECT == "clock":
+        clock = True
+        break
+    elif BOTW_SELECT == "password generator":
+        paswwordG = True
+        break
+if countdownS == True:
+    print("Welcome to countdown!")
+    def countdown(t):
+        while t:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins,secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1
+        print('Timer completed!')
+    #We ask the lengh
+    t = input('Enter the time in seconds: ')
+    #We print the timer
+    countdown(int(t))
+elif quiz == True:
+    print("Hello and welcome to quiz")
+    print("Do you need a reminder of the rules?")
+    get_rules = input("input y or n: ")
+    if get_rules == "y":
+        print("The rules are simple")
+        print("You are asked 3 questions")
+        print("You need to respond to them")
+        print("After responding all 3 questions you will get how many points you have!")
+        print("3 is best, 2 is better, 1 is ok, 0 is loss")
+        print("Try to get 3 points!")
+    else:
+        print("Ok!")
+    print("loading questions")
+    time.sleep(1.7)
+    print("selecting questions")
+    time.sleep(3)
+    question1 = "What does cpu stand for"
+    question2 = "What does ram stand for"
+    question3 = "In what year did micheal jackson died?"
+    question4 = "How many years did the second world war last?"
+    question5 = "What's the name of the cracker of the enigma code?"
+    question6 = "Who is the cofounder of microsoft?"
+    time.sleep(1.2)
+    print("questions loaded")
+    time.sleep(0.5)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    print("Herse the first question")
+    print(question1)
+    quest1Resp = input()
+    print("Herse the second question")
+    print(question2)
+    quest2Resp = input()
+    print("Herse the third question")
+    print(question3)
+    quest3Resp = input()
+    points = 0
+    if quest1Resp == "central proccecing unit" and quest2Resp == "random acces memory" and quest3Resp == "2009":
+        points += 3
+    elif quest1Resp == "central proceccing unit" and quest2Resp == "random acces memory" and quest3Resp != "2009":
+        points += 2
+    elif quest1Resp == "central proceccing unit" and quest2Resp != "random acces memory" and quest3Resp != "2009":
+        points += 1
+    elif quest1Resp != "central proceccing unit" and quest2Resp == "random acces memory" and quest3Resp != "2009":
+        points += 1
+    elif quest1Resp != "central proceccing unit" and quest2Resp != "random acces memory" and quest3Resp == "2009":
+        points += 1
+    elif quest1Resp != "central proceccing unit" and quest2Resp == "random acces memory" and quest3Resp == "2009":
+        points += 2
+    elif quest1Resp == "central proceccing unit" and quest2Resp != "random acces memory" and quest3Resp == "2009":
+        points += 2
+    print("Your points are...")
+    print(str(points))
+    if points == 3:
+        print("You got maximun points")
+    elif points == 2:
+        print("You got medimu points")
+    elif points == 1:
+        print("You got ok points")    
+elif calculator == True:
+    print("Welcome to calculator")
+    print("Instert the first number")
+    first_number = int(input())
+    print("insert the second number")
+    second_number = int(input())
+    print("Insert the operation")
+    print("(the supported operation are addition, subtraction, multiplications, division)")
+    operation =  input()
+    if operation == "addition":
+        final = first_number + second_number
+    elif operation == "subtratcion":
+        final = first_number - second_number
+    elif operation == "multiplication":
+        final = first_number * second_number
+    elif operation == "division":
+        final = first_number / second_number
+    print("The result is " + str(final))
+elif converter == True:
+    print("Welcome to converter!")
+    print("Selcect the opeartion")
+    print("Supported are: int to string, float to str")
+    select_conversion = input()
+    print("Plese insert the int/float")
+    if select_conversion == "int":
+        insert = int(input())
+    elif select_conversion == "float":
+        insert = float(input())
+    print("Converting")
+    time.sleep(0.5)
+    print("Conversion = " + str(insert))
+elif gtn == True:
+    print("Welcome to guess the number!")
+    print("Want to have a refresh on the rules?")
+    rules_refresh = input()
+    if rules_refresh == "y":
+        print("You have 3 attempts to get the number!")
+        print("The number is randomly genrated!")
+        print("Try to get it!")
+    elif rules_refresh == "n":
+        print("Okey!")
+    print("generating")
+    time.sleep(2)
+    print("Number generated")
+    print("Input a number between 1, 10")
+    number = random.randint(1, 10)
+    attempts = 3
+    responese = ["You got it first try", "Pretty good", "Saved on corner!"]
+    while attempts != 0:
+        numResp = int(input())
+        if numResp == number:
+            print("You won")
+            break
+        elif numResp < number:
+            print("Try again, and a little higer")
+            attempts -= 1
+        elif numResp > number:
+            print("Try again, and a little lower")
+            attempts -= 1
+        else:
+            print("Not a number between 1 and 10")
+            attempts -= 1
+    if attempts == 3:
+        print(responese[0])
+    elif attempts == 2:
+        print(responese[1])
+    elif attempts == 1:
+        print(responese[2])
+    else:
+        print("You lost")
+elif clock == True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+elif paswwordG == True:
+    print("Welcome to password generator!")
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    num = "0123456789"
+    special = "@#$%&*"
+    pass_len = int(input("Enter Password Length"))
+    print("Generating...")
+    time.sleep(2)
+    alpha_len = pass_len//2
+    num_len = math.ceil(pass_len*30/100)
+    special_len = pass_len-(alpha_len+num_len)
+    password = []
+    def generate_pass(length, array, is_alpha=False):
+        for i in range(length):
+            index = random.randint(0, len(array) - 1)
+            character = array[index]
+            if is_alpha:
+                case = random.randint(0, 1)
+                if case == 1:
+                    character = character.upper()
+            password.append(character)
+
+
+
+    generate_pass(alpha_len, alpha, True)
+    generate_pass(num_len, num)
+    generate_pass(special_len, special)
+    random.shuffle(password)
+    gen_password = ""
+    for i in password:
+        gen_password = gen_password + str(i)
+    print("Herse your password:")
+    print(gen_password)
